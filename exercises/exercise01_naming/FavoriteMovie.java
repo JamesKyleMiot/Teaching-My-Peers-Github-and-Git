a1fceb59b50c6cc38d935f90d3279979c861fe18
NELSON BALDADO <baldadonelson@gmail.com>
2026-03-21 19:48:34 +0800

    Fix class name and main method implementation
    Correct class name, method signature, and variable values.

--- exercises/exercise01_naming/FavoriteMovie.java
+++ exercises/exercise01_naming/FavoriteMovie.java
@@ -1,17 +1,17 @@
 package exercises.exercise01_naming;
 
-public class favoritEmovie {
+public class FavoriteMovie {
     //New excercise make sure to synx it first
 
     //Basic fixing
-    public static void rnain (String[] args) {
-        String name = "My Name";
+    public static void main (String[] args) {
+        String name = "NELSON";
 
-        int age = "0";
+        int age = 20;
 
-        boolean gender = 0;
+        boolean gender = true;
 
-        System.out.println(Name);
+        System.out.println(name);
         System.out.println(age);
         System.out.println(gender);
     }
