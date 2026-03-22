54ece869fe022e664e858fb5c299002a6a8864a7
japp24277 <jafetperez853@gmail.com>
2026-03-22 08:19:20 +0800

    Update Counter.java
    Solve

--- exercises/exercise07_loops/Counter.java
+++ exercises/exercise07_loops/Counter.java
@@ -3,7 +3,7 @@ package exercises.exercise07_loops;
 public class Counter {
     public static void main(String[] args) {
         // Task: Fix the error by adding the missing semicolon in the for loop.
-        for (int i = 1; i <= 5 i++) {
+        for (int i = 1; i < 5; i++) {
             System.out.println("Count: " + i);
         }
     }
